94eba830f5c341a507f67b68d8a73c0b2c1de1b2
Dan Bluhm Hansen <16237377+danbluhmhansen@users.noreply.github.com>
2022-10-03 20:57:13 +0200

    Migrated `/manage/index` page

---
+++ src/server/Content/pages/manage/index.tsx
@@ -0,0 +1,109 @@
+import { createRoot } from "react-dom/client";
+
+const hasPassword = true;
+const hasPhone = false;
+const hasTwoFactor = false;
+
+export default function Index() {
+  return (
+    <>
+      <h2 className="title">Manage your account</h2>
+      <h4 className="subtitle">Change your account settings</h4>
+      <table className="table">
+        <tbody>
+          <tr>
+            <td className="has-text-right">Password:</td>
+            <td>
+              {hasPassword ? (
+                <a
+                  href="/manage/changepassword"
+                  className="button is-link is-small"
+                >
+                  Change
+                </a>
+              ) : (
+                <a
+                  href="/manage/setpassword"
+                  className="button is-link is-small"
+                >
+                  Create
+                </a>
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="has-text-right">External Logins:</td>
+            <td>
+              <a
+                href="/manage/managelogins"
+                className="button is-link is-small"
+              >
+                Manage
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td className="has-text-right">Phone Number:</td>
+            <td>
+              {hasPhone ? (
+                <>
+                  <a
+                    href="/manage/addphonenumber"
+                    className="button is-link is-small"
+                  >
+                    Change
+                  </a>
+                  <a
+                    href="/manage/removephonenumber"
+                    className="button is-link is-small"
+                  >
+                    Remove
+                  </a>
+                </>
+              ) : (
+                <a
+                  href="/manage/addphonenumber"
+                  className="button is-link is-small"
+                >
+                  Add
+                </a>
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="has-text-right">Two-Factor Authentication:</td>
+            <td>
+              {hasTwoFactor ? (
+                <form
+                  method="post"
+                  action="/manage/disabletwofactorauthentication"
+                >
+                  <input
+                    type="submit"
+                    value="Disable"
+                    className="button is-link is-small"
+                  />
+                </form>
+              ) : (
+                <form
+                  method="post"
+                  action="/manage/enabletwofactorauthentication"
+                >
+                  <input
+                    type="submit"
+                    value="Enable"
+                    className="button is-link is-small"
+                  />
+                </form>
+              )}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+createRoot(document.querySelector("#container") as HTMLElement).render(
+  <Index />
+);
