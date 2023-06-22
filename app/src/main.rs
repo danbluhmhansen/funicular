3270102da2084fd547463cc744a09176d6f5a80b
Dan Bluhm Hansen <16237377+danbluhmhansen@users.noreply.github.com>
2023-06-22 22:34:36 +0200

    yew sample

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,4 +1,41 @@
+use gloo_net::http::Request;
+use serde::Deserialize;
 use yew::prelude::*;
+
+#[derive(Properties, PartialEq)]
+pub struct ButtonProps {
+    #[prop_or_default]
+    pub onclick: Callback<MouseEvent>,
+    pub children: Children,
+}
+
+#[function_component]
+fn Button(props: &ButtonProps) -> Html {
+    let class = classes!(
+        "px-4",
+        "py-2",
+        "rounded-md",
+        "dark:text-white",
+        "bg-cyan-500",
+        "hover:bg-cyan-600",
+        "active:bg-cyan-700",
+        "focus:outline-none",
+        "focus:ring",
+        "focus:ring-cyan-300",
+    );
+
+    html! {
+        <button onclick={&props.onclick} {class}>
+            { for props.children.iter() }
+        </button>
+    }
+}
+
+#[derive(Deserialize)]
+struct Character {
+    id: Option<String>,
+    name: Option<String>,
+}
 
 #[function_component]
 fn App() -> Html {
@@ -11,10 +48,28 @@ fn App() -> Html {
         }
     };
 
+    let characters: Vec<Character> = vec![];
+    let characters = use_state(|| characters);
+    {
+        let characters = characters.clone();
+        use_effect(move || {
+            wasm_bindgen_futures::spawn_local(async move {
+                if let Ok(response) = Request::get("http://localhost:3000/character").send().await {
+                    if let Ok(fetched_characters) = response.json::<Vec<Character>>().await {
+                        characters.set(fetched_characters);
+                    }
+                }
+            })
+        });
+    }
+
     html! {
         <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
+            <Button {onclick}>{ "+1" }</Button>
+            <p class={classes!("dark:text-white")}>{ *counter }</p>
+            <ul class={classes!("dark:text-white")}>
+                { characters.iter().filter_map(|c| c.name.as_ref() ).collect::<Html>() }
+            </ul>
         </div>
     }
 }
