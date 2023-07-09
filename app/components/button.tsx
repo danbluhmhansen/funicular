7906e5a062691edcbf41b4700d4277210bfcf999
Dan Bluhm Hansen <16237377+danbluhmhansen@users.noreply.github.com>
2023-07-09 17:52:55 +0200

    wip

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -8,8 +8,9 @@ export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
       disabled={!IS_BROWSER || props.disabled}
       class={`
         rounded-md px-4 py-2
+        flex flex-row items-center justify-center
         text-(white sm) font-medium
-        bg-blue-(700 hover:800 dark:600 dark:hover:700)
+        bg-blue-(700 hover:800 active:900 dark:600 dark:hover:700 dark:active:800)
         focus-(ring-4 ring-blue-300 outline-none dark:ring-blue-800)
       `}
     />
--- app/routes/games.tsx
+++ app/routes/games.tsx
@@ -1,7 +1,9 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
+import { Button } from "~components/button.tsx";
 import { Game } from "~api-models";
 import { Head } from "$fresh/runtime.ts";
 import funRequest from "~lib/funicular-request.ts";
+import { TbPlus } from "react-icons/tb";
 
 export const handler: Handlers<Game[]> = {
   async GET(_, ctx) {
@@ -16,28 +18,36 @@ export default function Page({ data }: PageProps<Game[]>) {
       <Head>
         <title>Funicular - Games</title>
       </Head>
-      {data.length > 0
-        ? (
-          <table class="table-auto border-collapse mx-auto">
-            <thead>
-              <tr class="px-4 py-2">
-                <th>Name</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((g) => (
-                <tr key={g.id} class="px-4 py-2">
-                  <td>
-                    <a href={`games/${g.name}`} class="hover:underline">
-                      {g.name}
-                    </a>
-                  </td>
+      <div class="mx-auto">
+        <div class="flex flex-row items-center justify-between space-x-4 p-4">
+          <Button>
+            <TbPlus class="mr-1" />
+            Add
+          </Button>
+        </div>
+        {data.length > 0
+          ? (
+            <table class="table-auto border-collapse mx-auto">
+              <thead>
+                <tr class="px-4 py-2">
+                  <th>Name</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        )
-        : <p>No games...</p>}
+              </thead>
+              <tbody>
+                {data.map((g) => (
+                  <tr key={g.id} class="px-4 py-2">
+                    <td>
+                      <a href={`games/${g.name}`} class="hover:underline">
+                        {g.name}
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )
+          : <p>No games...</p>}
+      </div>
     </>
   );
 }
