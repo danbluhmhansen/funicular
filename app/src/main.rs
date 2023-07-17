7620d36fe5959186bf7cdbffeea40ad41b310737
Dan Bluhm Hansen <16237377+danbluhmhansen@users.noreply.github.com>
2023-07-17 20:30:51 +0200

    routing

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,17 +1,28 @@
 use postgrest::Postgrest;
 use serde::Deserialize;
 use yew::prelude::*;
+use yew_router::prelude::*;
 
-#[derive(Clone, PartialEq, Deserialize)]
-struct Game {
-    id: Option<String>,
-    name: Option<String>,
-    created: Option<String>,
-    description: Option<String>,
+#[function_component]
+fn Header() -> Html {
+    html! {
+        <header class="mx-auto p-4">
+            <nav>
+                <ul class="flex flex-row justify-between space-x-8">
+                    <li>
+                        <Link<AppRoute> to={AppRoute::Home}>{ "Home" }</Link<AppRoute>>
+                    </li>
+                    <li>
+                        <Link<AppRoute> to={AppRoute::Games}>{ "Games" }</Link<AppRoute>>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    }
 }
 
 #[function_component]
-fn App() -> Html {
+fn Games() -> Html {
     let games = use_state(Vec::<Game>::new);
     {
         let games = games.clone();
@@ -36,19 +47,73 @@ fn App() -> Html {
     }
 
     html! {
+        <div class="mx-auto">
+            <h1>{"Games"}</h1>
+            {
+                if games.len() > 0 {
+                    html! {
+                        <table class="table-auto border-collapse mx-auto">
+                            <thead>
+                                <tr class="px-4 py-2">
+                                    <th>{"Name"}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {games.iter().map(|g|
+                                    html! {
+                                        <tr class="px-4 py-2">
+                                            <td>
+                                                {g.name.clone()}
+                                            </td>
+                                        </tr>
+                                    }
+                                ).collect::<Html>()}
+                            </tbody>
+                        </table>
+                    }
+                } else {
+                    html! {"No games..."}
+                }
+        }
+        </div>
+    }
+}
+
+#[derive(Clone, Routable, PartialEq)]
+enum AppRoute {
+    #[at("/")]
+    Home,
+    #[at("/games")]
+    Games,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(routes: AppRoute) -> Html {
+    match routes {
+        AppRoute::Home => html! { <h1>{"Home"}</h1> },
+        AppRoute::Games => html! { <Games /> },
+        AppRoute::NotFound => html! { <h1>{"Not found."}</h1> },
+    }
+}
+
+#[derive(Clone, PartialEq, Deserialize)]
+struct Game {
+    id: Option<String>,
+    name: Option<String>,
+    created: Option<String>,
+    description: Option<String>,
+}
+
+#[function_component]
+fn App() -> Html {
+    html! {
         <div class="flex flex-col min-h-screen">
-            <header class="mx-auto p-4">
-                <nav>
-                    <ul class="flex flex-row justify-between space-x-8">
-                        <li>
-                            <a href="/">{"Home"}</a>
-                        </li>
-                        <li>
-                          <a href="/games">{"Games"}</a>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
+            <BrowserRouter>
+                <Header />
+                <Switch<AppRoute> render={switch} />
+            </BrowserRouter>
         </div>
     }
 }
